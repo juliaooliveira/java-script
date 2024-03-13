accc0679b329b22f8e376055b714e209ead15946
juliaooliveira <148073511+juliaooliveira@users.noreply.github.com>
2024-03-13 09:32:07 -0300

    .

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 const botoes = document.querySelectorAll("botao");
-for(let i = 0; i < botoes.lenth;i++
+for(let i = 0; i < botoes.lenth;i++{
+    botoes.onclick = function(){
+        for(let j = 0;j < botoes.lenth;j++{
+            botoes.classlist.remove("ativo");
+        }
+        botoes[1].classlist.remove("ativo");        
+    }    
+}
