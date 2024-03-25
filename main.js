6631783471521aff87a9c83608623e196b01bbb5
juliaooliveira <148073511+juliaooliveira@users.noreply.github.com>
2024-03-25 08:49:59 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
-const botoes = document.querySelectorAll("botao");
-for(let i = 0; i < botoes.lenth;i++{
-    botoes.onclick = function(){
-        for(let j = 0;j < botoes.lenth;j++{
-            botoes.classlist.remove("ativo");
-        }
-        botoes[1].classlist.remove("ativo");        
-    }    
+const botoes = document ; querySelectorAll ("botao")
+const textos = document ; querySelectorAll (".aba-conteudo")
+
+for(let i=0 ; i < botoes.length ; i ++){
+botoes.onclick = function () {
+for( let j = 0 ; j < botoes.length ; j ++ ){
+botoes[j].classlist.remove( "ativo");
+textos[i].classlist.remove( "ativo");
 }
+botoes[i]. classlist. add ("ativo");
+textos[i]. classlist. add ("ativo");
+}
+}
+
+const cotadores = document.querySelectorAll(".contador");
+const tempoObjetivo1= new date ("2024-03-25T00:00:00");
