6d9b50869e36401f507ecd31f57f28e7282a86d7
juliaooliveira <148073511+juliaooliveira@users.noreply.github.com>
2024-05-02 07:39:51 -0300

    Update main.js

--- main.js
+++ main.js
@@ -35,16 +35,26 @@ function atualizaCronometro(){
   for(let i = 0 ,i < contadores.lenght; i++){
     contadores[i].textContent = calculaTempo(tempo[i]);
     }
+}
+
+function atualizaCronometro(){
+    document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
+    document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];
+    document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
+    document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
+
+    for (let i=0; i<contadores.length;i++){
+       // contadores[i].textContent = calculaTempo(tempos[i]); apagar essa linha
+        //document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
+        //document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
+        //document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
+        //document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];
+    }
+}
 
 function comecaCronometro(){
   atualizaCronometro();
   set Interval (atualizarCronometro, 1000);
 }
 comecaCronometro();
-  }
-if(tempoFinal>0){
-  return dias = 
-    }else{
-  return "Prazo finalizado";
-  }
-}
+  
